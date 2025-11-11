f745799223cb85c8727afabe94e45229cafb15c5
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 23:21:39 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  SHEET_URL_SCHEDULE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
-  SHEET_URL_HOLIDAY_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWkSisyPrrYRbZirLg6Xc9v1Z7eQHeO-aBNYbopP2pfqj5PAkqhznaYnmJzQh2H1PRnL_-GeMThYT/pub?output=csv",
-  API_URL: "https://script.google.com/macros/s/AKfycbxKec1QMXr3KOnds49rTD31GkCMyg0ipJCf2uRSWt_hpFYXXPAfJYnMPqnb7TkAEdpPsg/exec"
-};
