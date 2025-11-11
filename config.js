73fd2fe09f66eb365fd966bbde82f3ffa2839da4
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 20:24:08 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SHEET_URL_SCHEDULE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
+  SHEET_URL_HOLIDAY_CSV: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbzv4ot5A1WtjQhlngyY7A3oKQxo7Qz5JeOkM7Q8mXewcPFhPrgAyuuBzxsIlyRtKyTduw/exec"
+};
