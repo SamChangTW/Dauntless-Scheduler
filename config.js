2f98aa095267b3f5ba8aac1e4c9000e9928aafac
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 20:26:18 +0800

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SHEET_URL_SCHEDULE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
   SHEET_URL_HOLIDAY_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWkSisyPrrYRbZirLg6Xc9v1Z7eQHeO-aBNYbopP2pfqj5PAkqhznaYnmJzQh2H1PRnL_-GeMThYT/pub?output=csv",
-  API_URL: "https://script.google.com/macros/s/AKfycbzv4ot5A1WtjQhlngyY7A3oKQxo7Qz5JeOkM7Q8mXewcPFhPrgAyuuBzxsIlyRtKyTduw/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwMTDh49x7i7onUcsPoJ1kFPHYPJY4rmRgQ-wkGkJK3fFN3iWVUONknJBT1xFVqlkPA9w/exec"
 };
