79d7f867aa9db53b5c8967538953c0307f87445f
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 21:45:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const CONFIG={
   SHEET_URL_SCHEDULE_CSV:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
   SHEET_URL_HOLIDAY_CSV:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWkSisyPrrYRbZirLg6Xc9v1Z7eQHeO-aBNYbopP2pfqj5PAkqhznaYnmJzQh2H1PRnL_-GeMThYT/pub?output=csv",
-  API_URL:"REPLACE_WITH_YOUR_APPS_SCRIPT_WEBAPP_URL"
+  API_URL:"https://script.google.com/macros/s/AKfycbwMTDh49x7i7onUcsPoJ1kFPHYPJY4rmRgQ-wkGkJK3fFN3iWVUONknJBT1xFVqlkPA9w/exec"
 };
 const qs=s=>document.querySelector(s);const log=m=>{qs('#console').textContent+=`\n${m}`;}
 const z=n=>String(n).padStart(2,'0');const ymd=d=>`${d.getFullYear()}-${z(d.getMonth()+1)}-${z(d.getDate())}`;
