7f1515bd7f260bf4d8cc5e3be13bf86fa2f6a4d1
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 20:21:15 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Cloud + Deploy config
-window.DS_CONFIG = {
-  BASE_PATH: "/Dauntless-Scheduler/",
-  SITE_ORIGIN: "https://samchangtw.github.io",
-  REPO_URL: "https://samchangtw.github.io/Dauntless-Scheduler/",
-  API_URL: "https://script.google.com/macros/s/AKfycbyG-9dcgQyZ66tOGjmjwiaUOwM-VmKWaRM4AFjASDPF3r3ONP0ILPh_LeOA7sADcATZ/exec",
-  SHEET_URL_SCHEDULE_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
-  SHEET_URL_HOLIDAY_CSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWkSisyPrrYRbZirLg6Xc9v1Z7eQHeO-aBNYbopP2pfqj5PAkqhznaYnmJzQh2H1PRnL_-GeMThYT/pub?output=csv"
-};
