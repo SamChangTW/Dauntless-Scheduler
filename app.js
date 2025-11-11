1ac88e741d48214daf9f3971f6af4b5951a2ac8a
SamChangTW <164512235+SamChangTW@users.noreply.github.com>
2025-11-11 22:22:56 +0800

    Update API_URL in configuration

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const CONFIG={
   SHEET_URL_SCHEDULE_CSV:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5RM3eI5u486SbWyReQdEbBWMAsnsLKRwlL_FeFq8ejbV5b7JEgV2Wzf-mKref8YQhMXf4lXLvBAh/pub?output=csv",
   SHEET_URL_HOLIDAY_CSV:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQPWkSisyPrrYRbZirLg6Xc9v1Z7eQHeO-aBNYbopP2pfqj5PAkqhznaYnmJzQh2H1PRnL_-GeMThYT/pub?output=csv",
-  API_URL:"https://script.google.com/macros/s/AKfycbwMTDh49x7i7onUcsPoJ1kFPHYPJY4rmRgQ-wkGkJK3fFN3iWVUONknJBT1xFVqlkPA9w/exec"
+  API_URL:"https://script.google.com/macros/s/AKfycbxKec1QMXr3KOnds49rTD31GkCMyg0ipJCf2uRSWt_hpFYXXPAfJYnMPqnb7TkAEdpPsg/exec"
 };
 const qs=s=>document.querySelector(s);const log=m=>{qs('#console').textContent+=`\n${m}`;}
 const z=n=>String(n).padStart(2,'0');const ymd=d=>`${d.getFullYear()}-${z(d.getMonth()+1)}-${z(d.getDate())}`;
